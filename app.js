384ec7558b6898c371c9a8112acf6220e813825b
joeyWuTRKTR <godatascial@yahoo.com>
2021-09-11 10:47:17 +0800

    update: update north surf initialize page

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = process.env.port || 3000
 
+
+app.engine('hbs', exphbs({ dafaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('home page!')
+  res.render('index')
 })
+
 
 app.listen(port, () => {
   console.log('app is running in port: ', port)
